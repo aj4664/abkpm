b99788e8a904a4616b6419d27f7510daa306ff22
aj4664 <sjjwAjw4664@outlook.com>
2025-10-05 09:53:08 +0000

    add

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
     cd out123/cas
-    wget https://github.com/SukiSU-Ultra/SukiSU_KernelPatch_patch/releases/download/0.12.0/patch_linux
-    chmod +x patch_linux
     ./patch_linux
 find out123/cas -type f -name "*" | head -20
